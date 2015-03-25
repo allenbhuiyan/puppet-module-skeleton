a7cfd2bab540af3d8e26d5c11ed17b9c5d9bd8fb
Dave Kirk <david.kirk@bjss.com>
2015-03-25 15:00:59 +0000

    Removed files that are aimed at Open Source modules so I can use the skeleton in private projects too.
    Removed files are not actually *required* for an open source project and
    could easily be added to a module if wanted

--- skeleton/spec/spec_helper.rb
+++ skeleton/spec/spec_helper.rb
@@ -1,3 +1,9 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
+
+# Allow debugging of Puppet runs
+if ENV['DEBUG']
+  Puppet::Util::Log.level = :debug
+  Puppet::Util::Log.newdestination(:console)
+end
